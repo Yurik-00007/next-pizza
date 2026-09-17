a98acbe9c48467b7848f62fea9153c14b49bed10
Timofeev Yury <yurik-007@mail.ru>
2026-09-17 15:50:38 +0300

    start project

---
+++ app/product/[id]/page.tsx
@@ -0,0 +1,7 @@
+import App from "next/app";
+
+export default function ProductPage(app: App) {
+  return(
+    <div><h1>Product Page</h1></div>
+  )
+}
---
+++ app/product/page.tsx
@@ -0,0 +1,9 @@
+import App from "next/app";
+
+export default function ProductPage(app: App) {
+  return (
+    <div>
+      <h1>Product Page</h1>
+    </div>
+  );
+}
